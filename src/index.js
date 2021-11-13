7af0251f00937354cffe6a337612a7e3bd56e8ce
k1325699 <k1325699@gmail.com>
2021-11-13 22:48:09 +0800

    fix.把info放置側邊,feat.棋盤弄成棕色，winner遮罩，新增重新開始，

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './cssReset.css'
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
